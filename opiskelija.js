9991fe2847da9089d75d938a66161c9a89c74701
Meija Lohiniva <meija.lohiniva@seamk.fi>
2020-11-26 19:19:04 +0200

    Opiskelija päivitetty

--- opiskelija.js
+++ opiskelija.js
@@ -1,11 +1,22 @@
 // Konstruktorifunktio
-function Opiskelija(etunimi, sukunimi, aloitusvuosi) {
+function Opiskelija(etunimi, sukunimi, aloitusvuosi, sahkoposti) {
     this.etunimi = etunimi;
     this.sukunimi = sukunimi;
     this.aloitusvuosi = aloitusvuosi;
-    this.palutaKokonimi = function() {
-        return this.etunimi + ' ' + this.sukunimi;
+    this.sahkoposti = sahkoposti;
+
+    this.palautaTiedot = function() {
+        return this.etunimi + ' ' + this.sukunimi + ', aloittanut ' + this.aloitusvuosi + ' (sähköposti: ' + this.sahkoposti + ')';
+    }
+    this.tarkistaAloitusVuosi = function() {
+        if ( this.aloitusvuosi > 2020 ) {
+            console.log('Aloitusvuosi virheellinen');
+        }
     }
 }
 let opiskelija1 = new Opiskelija('Maija', 'Mallikas', 2010);
-console.log(opiskelija1.palutaKokonimi());
+opiskelija1.sahkoposti = 'maija.mallikas@joku.fi';
+console.log(opiskelija1.palautaTiedot());
+let opiskelija2 = new Opiskelija('Matti', 'Mallikas', 2022, 'matti@gmail.com');
+console.log(opiskelija2.palautaTiedot());
+opiskelija2.tarkistaAloitusVuosi();
