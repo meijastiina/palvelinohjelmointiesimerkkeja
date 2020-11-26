848c22ed3867bd891a08b5ab3953c2992d0c0d72
Meija Lohiniva <meija.lohiniva@seamk.fi>
2020-11-26 20:01:43 +0200

    hops

--- HOPS/hops.js
+++ HOPS/hops.js
@@ -3,6 +3,31 @@ function Opintojakso(nimi, laajuus, ajoitus) {
     this.nimi = nimi;
     this.laajuus = laajuus;
     this.ajoitus = ajoitus;
+    this.lisaaTaulukkoon = function() {
+        // Luodaan rivi
+        let uusiRivi = document.createElement('tr');
+        // Luodaan nimi-solu 
+        let uusiSolu = document.createElement('td');
+        // Lisätään solulle sisältö
+        uusiSolu.innerHTML = this.nimi;
+        // Lisätään solu riville
+        uusiRivi.appendChild(uusiSolu);
+        // Luodaan laajuus-solu 
+        uusiSolu = document.createElement('td');
+        // Lisätään solulle sisältö
+        uusiSolu.innerHTML = this.laajuus;
+        // Lisätään solu riville
+        uusiRivi.appendChild(uusiSolu);
+        // Luodaan ajoitus-solu 
+        uusiSolu = document.createElement('td');
+        // Lisätään solulle sisältö
+        uusiSolu.innerHTML = this.ajoitus;
+        // Lisätään solu riville
+        uusiRivi.appendChild(uusiSolu);
+        // Lisätään rivi tableen
+        document.getElementById('opintojaksoTable').appendChild(uusiRivi);
+        
+    }
 }
 
 // Lisää-painikkeen käsittely
@@ -10,4 +35,7 @@ document.getElementById('lisaaKurssi').onclick = function() {
     // Hae kentistä tiedot ja luodaan niiden perusteella uusi olio
     let ajoitusValinta = document.getElementById('ajoitus');
     let uusiOpintojakso = new Opintojakso(document.getElementById('nimi').value, document.getElementById('laajuus').value, ajoitusValinta[ajoitusValinta.selectedIndex].innerHTML);
+    // Kutsutaan luodun olion listaaTaulukkoon-metodia
+    uusiOpintojakso.lisaaTaulukkoon();
+    return false;
 }
